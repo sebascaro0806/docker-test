92a6cda6cb6c747c9571a94ffb429861fd347072
sc86 <sebascaro0806@gmail.com>
2022-11-15 12:20:15 -0500

    changed connection string

--- src/config/db.py
+++ src/config/db.py
@@ -1,6 +1,8 @@
 from sqlalchemy import MetaData, create_engine
 from src.config.setup import settings
 
-engine = create_engine("mysql+pymysql://" + settings.MYSQL_USER + ":" + settings.MYSQL_PASSWORD + "@" + settings.MYSQL_HOST + "/" + settings.MYSQL_DB)
+
+#host=base-imetrix.postgres.database.azure.com port=5432 dbname={your_database} user=admin2 password={your_password} sslmode=require
+engine = create_engine("postgresql://admin2:Imetrix2022@base-imetrix.postgres.database.azure.com:5432/postgres")
 
 meta_data = MetaData()
